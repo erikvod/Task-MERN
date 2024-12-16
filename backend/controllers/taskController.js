aa45ede763496fb744e6d4a7fbca31e5970c06c9
Erik Vodanovic <erikvod@gmail.com>
2024-12-15 23:48:58 -0500

    connected to mongo

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,22 +1,25 @@
-const getTasks = (req, res ) => {
-    res.status(200).json({ message: 'Get all tasks' });
-}
+const asyncHandler = require('express-async-handler');
 
-const createTask = (req, res) => {
+
+const getTasks = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Get all tasks' });
+});
+
+const createTask = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400);
         throw new Error('Please enter a task');
     }
     console.log(req.body);
     res.status(200).json({ message: 'Create a task' });
-}
+});
 
-const updateTask = (req, res) => {
+const updateTask = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Task ${req.params.id} updated` });
-}
+});
 
-const deleteTask = (req, res) => {
+const deleteTask = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Task ${req.params.id} deleted` });
-}
+});
 
 module.exports = { getTasks, createTask, updateTask, deleteTask };  // Export the controller functions
